6c99f59c4286bfb532c385fb12e7625cb9f7e8e6
MARTIN Loïck <298696350+Thymael@users.noreply.github.com>
2026-09-10 10:58:51 +0200

    Fiabilise le pipeline et prepare les donnees Power BI

--- fourcasters/models/marts/pbi_risque_incendie.sql
+++ fourcasters/models/marts/pbi_risque_incendie.sql
@@ -1,14 +1,23 @@
-/* Table prête pour Power BI : un danger prévu et la météo du jour de publication. */
 SELECT
     danger.id_danger_incendie,
     danger.reference_time,
     danger.date_publication,
     danger.date_prevision,
     danger.echeance,
+
     danger.numero_departement,
     departement.departement,
     departement.region,
+
     danger.niveau_danger,
+
+    meteo.date AS date_meteo_utilisee,
+
+    DATE_DIFF(
+        danger.date_publication,
+        meteo.date,
+        DAY
+    ) AS retard_meteo_jours,
 
     meteo.nombre_points_meteo,
     meteo.temperature_moyenne,
@@ -20,11 +29,20 @@ SELECT
     meteo.rafale_vent_maximale,
     meteo.couverture_nuageuse_moyenne,
     meteo.deficit_pression_vapeur_maximal,
-    meteo.nombre_points_meteo IS NOT NULL AS meteo_disponible
+
+    meteo.date IS NOT NULL AS meteo_disponible
 
 FROM {{ ref('fact_danger_incendie') }} AS danger
-LEFT JOIN {{ ref('int_meteo_departement_jour') }} AS meteo
-    ON danger.date_publication = meteo.date
-    AND danger.numero_departement = meteo.numero_departement
+
 INNER JOIN {{ ref('dim_departement') }} AS departement
     ON danger.numero_departement = departement.numero_departement
+
+LEFT JOIN {{ ref('int_meteo_departement_jour') }} AS meteo
+    ON danger.numero_departement = meteo.numero_departement
+    AND meteo.date <= danger.date_publication
+
+QUALIFY
+    ROW_NUMBER() OVER (
+        PARTITION BY danger.id_danger_incendie
+        ORDER BY meteo.date DESC
+    ) = 1
